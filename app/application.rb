c0b0ba2044d45ab6dcfc709185655e8ca9bf48ae
James Batsell <jamesbatsell05@gmail.com>
2020-04-11 00:07:20 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -17,7 +17,7 @@ class Application
       resp.write handle_search(search_term)
       
     elsif req.path.match(/cart/)
-      
+      resp.write "#"
       
     else
       resp.write "Path Not Found"
